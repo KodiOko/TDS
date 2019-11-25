b96a26f37089cb503c6f005fc41ea095487e3933
Vanden Herrewegen Guillaume <32937851+guivdh@users.noreply.github.com>
2019-11-25 13:19:24 +0100

    Create code.m

---
+++ code.m
@@ -0,0 +1,7 @@
+img = imread('voiture-rouge.jpg');
+    
+img=imsubtract(img(:,:,1),rgb2gray(img)); %soustrait le rouge de l'image puis transforme en échelle de gris
+img=medfilt2(img,[5,5]); %filtre qui permet de réduire le bruit de l'image
+img=imbinarize(img,0.25); %transforme l'image en image binaire pour n'avoir plus que du noir ou blanc
+img=bwareaopen(img,300); %permet de retirer tous les pixels blancs inférieur à 300px
+imshow(img);
