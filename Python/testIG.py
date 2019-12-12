e57d7a8ad07aba49d7a758afaf1e162882f47325
rebald <43856164+rebald@users.noreply.github.com>
2019-12-12 20:28:14 +0100

    interface graphique
    interface graphique bug

---
+++ Python/testIG.py
@@ -0,0 +1,59 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
+from separateur_createur_video import tracking
+
+class Root(Tk):
+
+    def __init__(self):
+        super(Root, self).__init__()
+        self.title("Tkinter Dialog Widget")
+        self.minsize(640,240)
+        #self.wm_iconbitmap('icon.ico')
+
+
+        self.labelFrame = ttk.LabelFrame(self, text = "Open A File")
+        self.labelFrame.grid(column = 0,row = 1, padx = 20, pady = 20)
+
+
+        self.button()
+        self.mybutton()
+        self.buttonDest()
+
+
+    def button(self):
+        self.button = ttk.Button(self.labelFrame, text = "choisir une vidéo", command = self.fileDialog)
+        self.button.grid(column = 1, row = 1)
+
+    def buttonDest(self):
+        self.button = ttk.Button(self.labelFrame, text = "choisir dossier destination", command = self.fileDialogDest)
+        self.button.grid(column = 2, row = 1)
+
+
+    def mybutton(self):
+        self.button = ttk.Button(self.labelFrame, text = "lancer le tracking",command = tracking(,))
+        self.button.grid(column = 3, row = 1)
+
+    def fileDialog(self):
+        
+        self.filename = filedialog.askopenfilename(initialdir = "C:\\", title = "Select A File", filetype= (("*.mp4","*.avi"), ("All files", "*.*")))
+
+        self.label = ttk.Label(self.labelFrame, text = "")
+        self.label.grid(column = 1,row = 2)
+        self.label.configure(text = self.filename)
+
+    def fileDialogDest(self):
+        
+        self.filename = filedialog.askdirectory(initialdir = "C:\\", title = "Select A Folder")
+        
+        self.label = ttk.Label(self.labelFrame, text = "")
+        self.label.grid(column = 1,row = 3)
+        self.label.configure(text = self.filename)
+
+
+
+if __name__== '__main__':
+    root = Root()
+    root.mainloop()
+ 
